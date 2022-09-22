0e5c62005e5e47726632eb79e37527f8789b4f93
felilozi <felicialozinski97@gmail.com>
2022-09-21 21:31:39 -0300

    vercion1

---
+++ proyecto_chocolateria/Appchoco/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from Appchoco.models import *
+# Register your models here.
+admin.site.register(Productos)
+admin.site.register(Usuario)
+admin.site.register(Contactos)
+
